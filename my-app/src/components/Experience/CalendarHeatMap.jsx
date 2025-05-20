38548eeac258920ad12cf23be71ccce74cd21c3a
Colin <kirbycolin26@gmail.com>
2025-05-19 22:59:25 -0400

    Inline Noting for CalendarHeatMap.jsx Done.

--- my-app/src/components/Experience/CalendarHeatMap.jsx
+++ my-app/src/components/Experience/CalendarHeatMap.jsx
@@ -1,3 +1,4 @@
+// Imports.
 import React from 'react';
 import { useState } from 'react';
 import { darken } from 'polished';
@@ -5,92 +6,98 @@ import styled from 'styled-components';
 import { motion, AnimatePresence} from 'framer-motion';
 import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
 
+// Gets Actual Numeric Date Array.
 const getDateRangeArray = (start, end) => {
-    const startDate = new Date(start);
-    const endDate = end.toLowerCase() === "present" ? new Date() : new Date(end);
-    const dates = [];
+    const startDate = new Date(start);                                                  // Get Start Date in Date Format.
+    const endDate = end.toLowerCase() === "present" ? new Date() : new Date(end);       // Get End Date in Date Format ( Handle "Present" Input).
+    const dates = [];                                                                   // Save Dates in Array.
 
-    let current = new Date(startDate);
+    let current = new Date(startDate);                                                  // Set Current Val to Start Date.
 
-    while (current <= endDate) {
-        dates.push(new Date(current));
-        current.setDate(current.getDate() + 1);
+    while (current <= endDate) {                                                        // While Current Date is Not End Date.
+        dates.push(new Date(current));                                                  // Add Current Date.
+        current.setDate(current.getDate() + 1);                                         // Increment Date.
     }
 
-    return dates;
+    return dates;                                                                       // Return Date Array.
 }
 
+// Groups Dates Array By Month W/ Reduce.
 const groupDatesByMonth = (dates) => {
-    return dates.reduce((acc, date) => {
-        const monthKey = `${date.getFullYear()}-${date.getMonth()}`
-        if(!acc[monthKey]) acc[monthKey] = [];
-        acc[monthKey].push(date);
-        return acc;
+    return dates.reduce((acc, date) => {                            // Reduce Method To Process Each Item In An Array.
+        const monthKey = `${date.getFullYear()}-${date.getMonth()}` // Creates Month Key From 4-Digit Year & Zero-Based Month.
+        if(!acc[monthKey]) acc[monthKey] = [];                      // If Bucket Doesn't Exist, Create Bucket.
+        acc[monthKey].push(date);                                   // Push Date Into Bucket By Month Key.
+        return acc;                                                 // Return Grouped Result, For Next Iteration.
     }, {});
 };
 
+// Checks If Working Dates Was A Full Year.
 const isFullYear = (dates,  year) => {
-    const start = new Date(`${year}-01-01`);
-    const end = new Date(`${year}-12-31`);
-    const daysInYear = (end - start) / (1000 * 60 * 60 * 24) + 1;
-    const daysInDates = dates.filter(date => date.getFullYear() === parseInt(year)).length;
-    return daysInDates >= daysInYear - 2;
+    const start = new Date(`${year}-01-01`);                                                   // Sets Start Date Of Year.
+    const end = new Date(`${year}-12-31`);                                                     // Sets End Date of Year.
+    const daysInYear = (end - start) / (1000 * 60 * 60 * 24) + 1;                              // Calculates All Days In A Calendar Year.
+    const daysInDates = dates.filter(date => date.getFullYear() === parseInt(year)).length;    // Counts How Many Dates In List Fall Within This Year.
+    return daysInDates >= daysInYear - 2;                                                      // Return If # Of Working Days Is Close Enough To A Full Year.
 }
 
+// CalendarHeatMap Component. Takes In Item.
 const CalendarHeatMap = ({ item }) => {
 
-    const start = item.duration.start;
-    const end = item.duration.end;
-    const dates = getDateRangeArray(start, end);
-    const months = groupDatesByMonth(dates);
+    const start = item.duration.start;                              // Sets 'start' To Start Date of Company (Item).
+    const end = item.duration.end;                                  // Sets 'end' To End Date of Company (Item).
+    const dates = getDateRangeArray(start, end);                    // References 'getDateRangeArray' Func & Sets Date Array of Company.
+    const months = groupDatesByMonth(dates);                        // Further Breaks Down Dates Array Into Month Sections.
 
-    const years = Object.keys(months).reduce((acc, key) => {
-        const [year] = key.split('-');
-        if(!acc.includes(year)) acc.push(year);
-        return acc;
-    }, []);
+    const years = Object.keys(months).reduce((acc, key) => {        // Reduce Using Months.
+        const [year] = key.split('-');                              // Gets Year Key From 'YYYY-MM' Formatted String.
+        if(!acc.includes(year)) acc.push(year);                     // If Bucket Doesn't Exist, Add Year To Final Result.
+        return acc;                                                 // Return Array Of Unique Years.
+    }, []);                                                         // Start W/ An Empty Array.
 
-    const [expandedYears, setExpandedYears] = useState(() => {
-        const currentYear = new Date().getFullYear().toString();
-        return [currentYear];
+    const [expandedYears, setExpandedYears] = useState(() => {      // UseState To Update Years That Should Be Expanded.
+        const currentYear = new Date().getFullYear().toString();    // On Load Get Current Year.
+        return [currentYear];                                       // On Load Current Year Is Only Current Year.
     })
     
-    const toggleYear = (year) => {
-        setExpandedYears(prev =>
-            prev.includes(year)
-            ? prev.filter(y => y !== year)
-            : [...prev, year]
+    const toggleYear = (year) => {                                  // Toggle Year Func For 'onClick' Of Expand Icon.
+        setExpandedYears(prev =>                                    // Use setExpandedYears' State.
+            prev.includes(year)                                     // Return Boolean For State Including Input Year (T or F).
+            ? prev.filter(y => y !== year)                          // If So, Remove From Expanded Years.
+            : [...prev, year]                                       // Else, Add To Expanded Years.
         );
     };
 
-    const getAllDaysInMonth = (year, month) => {
-        const date = new Date(year, month, 1);
-        const result = [];
+    const getAllDaysInMonth = (year, month) => {                    // Func To Get All Days In Actual Date Month.
+        const date = new Date(year, month, 1);                      // Create Date At First Date of Month.
+        const result = [];                                          // Initialize Empty Result.
 
-        while (date.getMonth() === month) {
-            result.push(new Date(date));
-            date.setDate(date.getDate() + 1);
+        while (date.getMonth() === month) {                         // Keep Looping While In Same Month.
+            result.push(new Date(date));                            // Add Current Date To Month.
+            date.setDate(date.getDate() + 1);                       // Increment Day Up One.
         }
 
-        return result;
+        return result;                                              // Return Days In Month Array.
     }
 
     return (
         <HeatMapContainer>
-          {years.map((year) => {
-            const yearDates = dates.filter((d) => d.getFullYear() === parseInt(year));
-      
+            {years.map((year) => {  { /* Creates Map From Years. */ }
             return (
-              <YearGroup key={year}>
-                <YearRow 
+              <YearGroup key={year}>    { /* Sets Up Each Year */ }
+                { /* Year Bar. E.g. Bar Containing 2024 & Arrow Icon */ }
+                <YearRow
                     onClick={() => toggleYear(year)}
                     $bg={item.theme.soft}
                 >
-                  <YearLabel>{year}</YearLabel>
-                  {isFullYear(dates, year)}
-                  <Arrow>{expandedYears.includes(year) ? <FaChevronUp /> : <FaChevronDown />}</Arrow>
+                    { /* Sets Year Label. E.g. 2024 */ }
+                    <YearLabel>{year}</YearLabel>
+                    { /* Sets Arrow Icon W/ Expanding Logic. */ }
+                    <Arrow>{expandedYears.includes(year) ? <FaChevronUp /> : <FaChevronDown />}</Arrow>
                 </YearRow>
+                { /* Animates Expaneded Month Grid. */ }
                 <AnimatePresence initial={false}>
+                    { /* Only Show Expanded Years W/ Proper Animated Expansion. */ }
                     {expandedYears.includes(year) && (
                         <MotionMonthGridWrap
                             key={year}
@@ -99,6 +106,7 @@ const CalendarHeatMap = ({ item }) => {
                             exit={{ scaleY: 0, opacity: 0 }}
                             transition={{duration: 0.2, ease: "easeInOut"}}
                         >
+                            { /* Looping Through Months Per Year & Extracts Month Index & Sets Up To Render Each Month */ }
                             {Object.entries(months)
                             .filter(([key]) => key.startsWith(year))
                             .map(([key, days]) => {
@@ -112,8 +120,9 @@ const CalendarHeatMap = ({ item }) => {
                                     key={key}
                                     $bg={item.theme.mutedBackground}    
                                 >
-                                    <MonthTitle>{monthName} {year}</MonthTitle>
+                                    <MonthTitle>{monthName} {year}</MonthTitle> { /* Displays Short Month Name & Year. E.g. Jan 2024. */ }
                                     <MonthGrid>
+                                    { /* Displays Day Blocks & Sets Block To Clear Or Not Depending On If It Was Worked */ }
                                     {getAllDaysInMonth(parseInt(year), parseInt(monthIndex)).map((date, j) => {
                                         const isWorked = date >= new Date(item.duration.start) &&
                                             date >= new Date(item.duration.start) &&
@@ -135,7 +144,7 @@ const CalendarHeatMap = ({ item }) => {
                                                     $outline={!isWorked}
                                                 />
                                             );
-                                })}
+                                    })}
                                     </MonthGrid>
                                 </MonthBlock>
                                 );
@@ -158,14 +167,12 @@ const HeatMapContainer = styled.div`
     gap: 2rem;
     width: 100%;
 `;
-
 // ------------ Individual Year & Month Containers ------------
 const YearGroup = styled.div`
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
 `;
-
 // ------------ Year Bars ------------
 const YearRow = styled.div`
     display: grid;
@@ -182,7 +189,6 @@ const YearRow = styled.div`
         background: ${( { $bg }) => darken(0.07, $bg)};
     }
 `;
-
 // Year Label. E.g. 2025.
 const YearLabel = styled.div`
     font-weight: bold;
@@ -190,7 +196,19 @@ const YearLabel = styled.div`
     text-align: center;
     justify-self: center;
 `;
+// Expanding Arrows.
+const Arrow = styled.div`
+    display: flex;
+    font-size: 0.8rem;
+    color: white;
+    margin-left: auto;
+    margin-right: 0.2em;
 
+    &:hover {
+        opacity: 0.5;
+        cursor: pointer;
+    }
+`;
 // ------------ Month Grid ------------
 const MotionMonthGridWrap = styled(motion.div)`
     display: grid;
@@ -203,7 +221,6 @@ const MotionMonthGridWrap = styled(motion.div)`
     transition: 0.5s ease;
     transform-origin: top;
 `;
-
 // Each Individual Month Block.
 const MonthBlock = styled.div`
     background: ${ (props) => props.$bg };
@@ -219,20 +236,20 @@ const MonthBlock = styled.div`
         box-shadow: 0 2px 6px rgba(0, 0, 0, 0.5);
     }
     
-`
+`;
 // Month Titles. E.g. Jan, Feb, Mar.
 const MonthTitle = styled.h4`
     margin: 0 0 0.3rem 0;
     font-size: 0.8rem;
     color: white;
-`
+`;
 // Whole Grid for Day Blocks.
 const MonthGrid = styled.div`
     display: grid;
     grid-template-columns: repeat(7, 10px);
     grid-auto-rows: 10px;
     gap: 2px;
-`
+`;
 // Individual Day Blocks.
 const Block = styled(motion.div)`
     width: 10px;
@@ -241,19 +258,7 @@ const Block = styled(motion.div)`
     opacity: 0.85;
     border-radius: 2px;
     border: ${({ $outline }) => ($outline ? '1px solid rgba(255, 255, 255, 0.5)' : 'none')};
-`
-// Expanding Arrows.
-const Arrow = styled.div`
-    display: flex;
-    font-size: 0.8rem;
-    color: white;
-    margin-left: auto;
-    margin-right: 0.2em;
-
-    &:hover {
-        opacity: 0.5;
-        cursor: pointer;
-    }
 `;
 
+// Export CalendarHeatMap Component.
 export default CalendarHeatMap;
